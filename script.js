43911a4ced5c4a31b0806a8f8638142476dea3b4
AntonShiftd <anton.domrachev.95@mail.ru>
2024-08-12 22:05:11 +0300

    commit-rework-app

--- script.js
+++ script.js
@@ -11,58 +11,66 @@ function btnOnclick(){allBtn.forEach((item)=>{
     else if(classlt === 'division'||classlt === 'mult'||classlt === 'minus'||classlt === 'plus'){
         item.addEventListener('click', ()=>symb(item))
     }
+    else if(classlt === 'procent'){
+        item.addEventListener('click', ()=>procent(item))
+    }
     else if(classlt === 'equals'){
         item.addEventListener('click', ()=>results(item))
     }
     else if(classlt === 'fr'){
         item.addEventListener('click', fr)
     }
-    // else if(classlt === 'plusMinus'){
-    //     item.addEventListener('click', plusMinus)
-    // }
+    else if(classlt === 'plusMinus'){
+        item.addEventListener('click', plusMinus)
+    }
 })}
 
 
 //Функционал цифр
 function num(item){
+        statusCalc.statusState = false
         printDisplay.NumArr.push(item.innerHTML)
-        printDisplay.displayNum =Number(`${printDisplay.displayNum}` + `${printDisplay.NumArr[statusCalc.numTap]}`)
+        printDisplay.displayNum =Number(`${printDisplay.displayNum}` + `${printDisplay.NumArr[printDisplay.NumArr.length-1]}`)
         display.innerHTML = printDisplay.displayNum
-        statusCalc.numTap++
-        console.log('num')
+        console.log(printDisplay.displayNum)
         if(statusCalc.statusFr === true){
+            if(printDisplay.result === statusCalc.memory[statusCalc.memory.length-1]){
+                printDisplay.displayNum = Number(`${printDisplay.result}` + `${printDisplay.displayNum}`)/10
+            }
+            else{
             printDisplay.displayNum = printDisplay.displayNum/10
+            }
             display.innerHTML = printDisplay.displayNum
             statusCalc.statusFr = false
         }
 }
+        
 
 
 function symb(item){
-    statusCalc.statusFr = false
-    statusCalc.memory.push(printDisplay.displayNum)
-    statusCalc.state.push(item.innerHTML)
-    statusCalc.numTap = 0
-    if(statusCalc.memory.length === 2){
-        operation(statusCalc.memory[0], statusCalc.memory[1])
-        display.innerHTML = printDisplay.result
+   switch(statusCalc.statusState){
+   case true: {
+        statusCalc.state.pop()
+        statusCalc.state.push(item.innerHTML)
+        break
     }
-    else if(statusCalc.memory.length>=2 && statusCalc.statusEquals === true){
-        display.innerHTML = printDisplay.result
-        statusCalc.statusEquals = false
-        console.log(statusCalc)
-        console.log(printDisplay)
-    }
-    else if(statusCalc.memory.length>2){
-        operation(printDisplay.result, statusCalc.memory[statusCalc.memory.length-2])
-        display.innerHTML = printDisplay.result
-    }
-    printDisplay.NumArr = []
-    printDisplay.displayNum = 0
-    console.log(typeof(printDisplay.result))
-}
+    case false:{
+        statusCalc.statusState = true
+        statusCalc.memory.push(printDisplay.displayNum)
+        printDisplay.NumArr = []
+        statusCalc.state.push(item.innerHTML)
+        if(statusCalc.memory.length === 2){
+            operation(statusCalc.memory[0], statusCalc.memory[1])
+            display.innerHTML = printDisplay.result
+            statusCalc.memory = [printDisplay.result]
+            statusCalc.result = 0
+            }
+        printDisplay.displayNum = 0
+        printDisplay.numTap = 0
+   }}}
 
-function operation(fn, sn){
+
+   function operation(fn, sn){
     if(statusCalc.state[statusCalc.state.length - 2] == '÷'){
         printDisplay.result = fn / sn
     }
@@ -82,66 +90,121 @@ function operation(fn, sn){
     statusCalc.statusFr = true
  }
 
+ //Функционал +-
+ function plusMinus(){
+    statusCalc.memory.push(printDisplay.displayNum*(-1))
+    printDisplay.displayNum = statusCalc.memory[statusCalc.memory.length-1]
+    display.innerHTML = statusCalc.memory[statusCalc.memory.length-1]
+    if(statusCalc.memory.length>0){
+    statusCalc.memory[statusCalc.memory.length-1] = statusCalc.memory[statusCalc.memory.length-2]
+    statusCalc.memory.shift()}}
 
-//  //Функционал +-
-//  function plusMinus(){
-//     statusCalc.memory.pop()
-//         if(printDisplay.displayNum === 0){
-//             printDisplay.result = printDisplay.result*(-1)
-//             display.innerHTML = printDisplay.result
-//             statusCalc.memory.push(printDisplay.result)
-//         }
-//         else{
-//         printDisplay.displayNum = printDisplay.displayNum*(-1)
-//         display.innerHTML = printDisplay.displayNum
-//         statusCalc.memory.push(printDisplay.displayNum)
-//         }
-//         console.log(statusCalc)
-//         console.log(printDisplay)
-//     }
-
- //Функционал =
+    //Функционал =
  function results(item){
+    statusCalc.statusState = false
     statusCalc.statusEquals = true
     symb(item)
-    statusCalc.memory.push(printDisplay.result)
-    statusCalc.state.pop()
-    
+    statusCalc.memory = [printDisplay.result]
+    statusCalc.state = []
  }
 
-//Функционал кнопки AC
+ //Функционал кнопки AC
 function clean(){
     display.innerHTML = 0
-    statusCalc.numTap = 0
     statusCalc.state = []
     statusCalc.memory = []
     statusCalc.statusFr = false
-    printDisplay.resultArr = []
+    statusCalc.statusState = false
     printDisplay.result = 0
     printDisplay.NumArr = []
     printDisplay.displayNum = 0
-    
     }
 
+    //Функционал %
+function procent(item){
+    statusCalc.memory.push(printDisplay.displayNum)
+    printDisplay.displayNum = statusCalc.memory[statusCalc.memory.length-2]/100*statusCalc.memory[statusCalc.memory.length-1]
+    statusCalc.memory.push(printDisplay.displayNum)
+    statusCalc.memory[statusCalc.memory.length-2] = statusCalc.memory[statusCalc.memory.length-3]
+    statusCalc.memory.shift()
+    statusCalc.state.push(item.innerHTML)
+    operation(statusCalc.memory[statusCalc.state.length-2],statusCalc.memory[statusCalc.state.length-1])
+    statusCalc.state.pop()
+    display.innerHTML = printDisplay.result
+    statusCalc.memory = [printDisplay.result]
+    statusCalc.state = []
+}
 
     //Переменные
     const allBtn = document.querySelectorAll('.btn')
     const display = document.querySelector('.calc-screen')
     const statusCalc = {
-        numTap: 0,
         state: [],
         memory: [],
+        statusState: false,
         statusEquals: false,
         statusFr: false,
     }
     const printDisplay = {
-        resultArr:[],
         NumArr:[],
         result: 0,
         displayNum: 0,
     }
     
     btnOnclick()
+
+
+//     case false: {
+//     statusCalc.statusFr = false
+//     statusCalc.memory.push(printDisplay.displayNum)
+//     statusCalc.state.push(item.innerHTML)
+//     statusCalc.numTap = 0
+//     if(statusCalc.memory.length === 2){
+//         operation(statusCalc.memory[0], statusCalc.memory[1])
+//         display.innerHTML = printDisplay.result
+//     }
+//     else if(statusCalc.memory.length>=2 && statusCalc.statusEquals === true){
+//         operation(printDisplay.result, statusCalc.memory[statusCalc.memory.length-1])
+//         display.innerHTML = printDisplay.result
+//         statusCalc.statusEquals = false
+//         console.log(statusCalc)
+//         console.log(printDisplay)
+//     }
+//     else if(statusCalc.memory.length>=2 && statusCalc.statusEquals === false){
+//         operation(printDisplay.result, statusCalc.memory[statusCalc.memory.length-1])
+//         display.innerHTML = printDisplay.result
+//     }
+//     statusCalc.statusState = true
+//     printDisplay.NumArr = []
+//     printDisplay.displayNum = 0}
+// }}
+
+
+
+
+
+
+
+
+ 
+    
+    //     if(printDisplay.displayNum === 0){
+    //         printDisplay.result = printDisplay.result*(-1)
+    //         display.innerHTML = printDisplay.result
+    //     }
+    //     else{
+    //     printDisplay.displayNum = printDisplay.displayNum*(-1)
+    //     display.innerHTML = printDisplay.displayNum
+    //     }
+    //     console.log(statusCalc)
+    //     console.log(printDisplay)
+
+
+ 
+
+
+
+    
 
 
 
@@ -267,113 +330,3 @@ function noErrNum(item){
     }
 }
 */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let a = ''
-// let b = ''
-// let c = ''
-// let sumbol = ''
-// let results = false
-
-// const num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.']
-// const action = ['−', '+', '×', '÷'] 
-// //Вывод
-// const out = document.querySelector('.calc-screen')
-// //Функция сброса
-// function clear(){
-//     a = ''
-//     b = ''
-//     c = ''
-//     sumbol = ''
-//     results = false
-//     out.textContent = 0
-// }
-
-// document.querySelector('.AC').onclick = clear
-
-// let buttonList = document.querySelectorAll('.btn')
-// buttonList.forEach(function(btn){
-//     if(btn.innerHTML === 'AC'){return}
-//     else{
-//     btn.addEventListener('click', eventBtn)}})
-
-//     function eventBtn(){
-//         const key = event.target.textContent
-//         console.log(key)
-//         if(num.includes(key)){
-//             if (b ==='' && sumbol === ''){
-//                 a += key
-//                 out.textContent = a
-//                 console.log(`a = ${a}`)
-//             }
-//             else if(a!=='' && b!=='' && results) {
-//                 b = key
-//                 results = false
-//                 out.textContent = b
-//                 console.log(`b = ${b}`)
-//             }
-//             /* else if(a!=='' && b!=='' && c!=='' && sumbol!=='')
-//                 {
-//                     out.textContent = c
-//                     a = c
-//                     b += key
-//                 out.textContent = b} */
-//             else{
-//                 b += key
-//                 out.textContent = b
-//                 console.log(`b = ${b}`)
-//             }
-//             return
-//         }
-        
-//     if(action.includes(key)){
-//     sumbol = key
-//     out.textContent = sumbol
-//     console.log(`sumbol = ${sumbol}`)
-//     return
-//     }
-
-//     if (key === '=') {
-//         if(b === '') {b = a}
-//         switch(sumbol) {
-//             case '+':
-//                 c = (+a) + (+b)
-//                 break
-//             case '−':
-//                 c = a - b
-//                 break
-//             case '×':
-//                 c = a * b
-//                 break
-//             case '÷':
-//                 if (b === '0') {
-//                     out.textContent = 'No'
-//                     a = ''
-//                     b = ''
-//                     sumbol = ''
-//                     return
-//                 }
-//                 c = a / b
-//                 break
-//         }   
-//         results = true
-//         out.textContent = c
-//     }
-// }
